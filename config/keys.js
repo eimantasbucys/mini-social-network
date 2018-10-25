d1f126d1f521f78c0625ec2b550af75eedee3810
Bucis <eimantas@bucys.eu>
2018-10-25 16:04:09 +0300

    .

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://ebucys:spalis123@ds155730.mlab.com:55730/devnetwork"
+  mongoURI: "mongodb://ebucys:spalis123@ds155730.mlab.com:55730/devnetwork",
+  secretOrKey: "secret"
 };
